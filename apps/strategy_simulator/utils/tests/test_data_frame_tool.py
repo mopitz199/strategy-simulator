cd3f7f42e79b5d9a2ebbf5a5bed2d02a1321c6bf
Maximiliano Opitz <mopitz199@gmail.com>
2023-07-12 19:01:05 -0400

    test(utils): testing the add_trend method

--- apps/strategy_simulator/utils/tests/test_data_frame_tool.py
+++ apps/strategy_simulator/utils/tests/test_data_frame_tool.py
@@ -1,3 +1,4 @@
+import pytest
 import pandas
 
 from utils.utils.data_frame_tool import DataFameTool
@@ -5,9 +6,28 @@ from utils.utils.data_frame_tool import DataFameTool
 
 class TestDataFrameToolAddEma:
     def test_add_ema(self):
-        d = {"col1": [1, 2], "col2": [3, 4]}
-        data_frame = pandas.DataFrame(data=d)
+        data = {"col1": [1, 2], "col2": [3, 4]}
+        data_frame = pandas.DataFrame(data=data)
 
         tool = DataFameTool(data_frame=data_frame)
         tool.add_ema(ema_period=55, reference_column_name="col2", ema_column_name="ema")
         assert list(tool.data_frame.columns) == ["col1", "col2", "ema"]
+
+
+class TestDataFrameToolAddTrend:
+    @pytest.mark.parametrize(
+        "data,result",
+        [
+            ([1, 5, 4, 1, 3], ["none", "none", "bullish", "bearish", "bearish"]),
+            ([1, 5, 4, 1, 4], ["none", "none", "bullish", "bearish", "lateral"]),
+        ],
+    )
+    def test_add_trend(self, data, result):
+        data = {"ema": data}
+        data_frame = pandas.DataFrame(data=data)
+
+        tool = DataFameTool(data_frame=data_frame)
+        tool.add_trend(
+            number_of_registers=2, ema_column_name="ema", trend_column_name="trend"
+        )
+        assert tool.data_frame["trend"].tolist() == result
--- apps/strategy_simulator/utils/utils/data_frame_tool.py
+++ apps/strategy_simulator/utils/utils/data_frame_tool.py
@@ -1,29 +1,33 @@
 from utils import constants as utils_constants
 
-class DataFameTool:
 
+class DataFameTool:
     def __init__(self, data_frame):
         self.data_frame = data_frame
 
     def add_ema(
-            self,
-            ema_period: int,
-            reference_column_name: str,
-            ema_column_name: str,
-        ):
-        self.data_frame[ema_column_name] = self.data_frame[reference_column_name].ewm(
-            span=ema_period, adjust=False
-        ).mean()
+        self,
+        ema_period: int,
+        reference_column_name: str,
+        ema_column_name: str,
+    ):
+        self.data_frame[ema_column_name] = (
+            self.data_frame[reference_column_name]
+            .ewm(span=ema_period, adjust=False)
+            .mean()
+        )
 
-    def add_trend(self, number_of_registers, ema_column_name: str, trend_column_name: str):
+    def add_trend(
+        self, number_of_registers, ema_column_name: str, trend_column_name: str
+    ):
         if ema_column_name not in list(self.data_frame.columns):
             raise Exception("Ema column does not exists")
         else:
-            trends = ["None"]*number_of_registers
+            trends = ["none"] * number_of_registers
             ema_values = self.data_frame[ema_column_name].tolist()
 
-            for index in range(number_of_registers, len(ema_values)-1):
-                start_ema_value = ema_values[index-number_of_registers]
+            for index in range(number_of_registers, len(ema_values)):
+                start_ema_value = ema_values[index - number_of_registers]
                 end_ema_value = ema_values[index]
 
                 if start_ema_value > end_ema_value:
@@ -34,4 +38,4 @@ class DataFameTool:
                     trend = utils_constants.LATERAL_TREND
                 trends.append(trend)
 
-            self.data_frame[trend_column_name] = trends   
+            self.data_frame[trend_column_name] = trends
