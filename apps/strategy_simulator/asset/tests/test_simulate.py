c9247035d7bdcd84f44207df7b2430b6f2b323b8
Maximiliano Opitz <mopitz199@gmail.com>
2023-07-18 19:30:47 -0400

    feature(simulate): creating the logic to process each day in the strategy simulation

---
+++ apps/strategy_simulator/asset/tests/test_simulate.py
@@ -0,0 +1,197 @@
+import pandas
+import pytest
+
+from unittest.mock import patch
+
+from asset.use_cases.simulate import *
+
+
+class TestProcessIndexFunctions:
+    @pytest.mark.parametrize(
+        "value, result",
+        [
+            (pandas.Timestamp("2023-01-01"), "2023-01-01"),
+        ],
+    )
+    def test_process_timestamp_to_date_index(self, value, result):
+        process_value = ProcessIndexFunctions.process_timestamp_to_date_index(value)
+        assert process_value == result
+
+
+class TestSimulate:
+    def test_build_index(self):
+        data = {
+            "date": [pandas.Timestamp("2023-01-01"), pandas.Timestamp("2023-04-15")]
+        }
+        simulate = Simulate(None)
+        simulate.data_frame = pandas.DataFrame(data=data)
+        simulate.build_index(
+            column_name="date",
+            process_index_function=ProcessIndexFunctions.process_timestamp_to_date_index,
+        )
+        assert simulate.data_index == {"2023-01-01": 0, "2023-04-15": 1}
+
+    @pytest.mark.parametrize(
+        "row,result",
+        [
+            (
+                {
+                    "ema": Decimal("29"),
+                    "high": Decimal("100"),
+                    "low": Decimal("30"),
+                    "open": Decimal("60"),
+                },
+                Decimal("30.16"),
+            ),
+            (
+                {
+                    "ema": Decimal("28"),
+                    "high": Decimal("100"),
+                    "low": Decimal("30"),
+                    "open": Decimal("60"),
+                },
+                None,
+            ),
+            (
+                {
+                    "ema": Decimal("80"),
+                    "high": Decimal("100"),
+                    "low": Decimal("30"),
+                    "open": Decimal("60"),
+                },
+                Decimal("60"),
+            ),
+            (
+                {
+                    "ema": Decimal("50"),
+                    "high": Decimal("100"),
+                    "low": Decimal("30"),
+                    "open": Decimal("60"),
+                },
+                Decimal("52"),
+            ),
+        ],
+    )
+    def test_get_bullish_purchase_price(self, row, result):
+        simulate = Simulate(
+            {
+                "bullish_attractive_percentage": Decimal("0.04"),
+            }
+        )
+
+        bulllish_purchase_price = simulate.get_bullish_purchase_price(row=row)
+        assert bulllish_purchase_price == result
+
+    @pytest.mark.parametrize(
+        "row,result",
+        [
+            (
+                {
+                    "ema": Decimal("70"),
+                    "high": Decimal("100"),
+                    "low": Decimal("30"),
+                    "open": Decimal("60"),
+                },
+                Decimal("60"),
+            ),
+            (
+                {
+                    "ema": Decimal("35"),
+                    "high": Decimal("100"),
+                    "low": Decimal("30"),
+                    "open": Decimal("60"),
+                },
+                Decimal("32.55"),
+            ),
+            (
+                {
+                    "ema": Decimal("200"),
+                    "high": Decimal("100"),
+                    "low": Decimal("30"),
+                    "open": Decimal("60"),
+                },
+                Decimal("60"),
+            ),
+            (
+                {
+                    "ema": Decimal("30"),
+                    "high": Decimal("100"),
+                    "low": Decimal("30"),
+                    "open": Decimal("60"),
+                },
+                None,
+            ),
+        ],
+    )
+    def test_get_bearish_purchase_price(self, row, result):
+        simulate = Simulate(
+            {
+                "bearish_attractive_percentage": Decimal("-0.07"),
+            }
+        )
+
+        bulllish_purchase_price = simulate.get_bearish_purchase_price(row=row)
+        assert bulllish_purchase_price == result
+
+    @pytest.mark.parametrize(
+        "purchase_price,result",
+        [
+            (Decimal("100"), Decimal("10")),
+            (Decimal("150"), Decimal("6.67")),
+        ],
+    )
+    def test_get_number_of_assets_to_buy(self, purchase_price, result):
+        simulate = Simulate(
+            {
+                "not_invested_amount": Decimal("1000"),
+            }
+        )
+        number_of_assets = simulate.get_number_of_assets_to_buy(
+            purchase_price=purchase_price
+        )
+        assert number_of_assets == result
+
+    @pytest.mark.parametrize(
+        "date,trend,purchase_price,number_of_assets,not_invested_amount",
+        [
+            ("2023-01-02", "bullish", Decimal("120"), Decimal("8.33"), Decimal("0")),
+            ("2023-01-01", "bullish", Decimal("120"), Decimal("10"), Decimal("0")),
+            ("2023-01-02", "bullish", None, Decimal("0"), Decimal("1000")),
+            ("2023-01-01", "bullish", None, Decimal("0"), Decimal("1200")),
+            ("2023-01-02", "bearish", Decimal("120"), Decimal("8.33"), Decimal("0")),
+            ("2023-01-01", "bearish", Decimal("120"), Decimal("10"), Decimal("0")),
+            ("2023-01-02", "bearish", None, Decimal("0"), Decimal("1000")),
+            ("2023-01-01", "bearish", None, Decimal("0"), Decimal("1200")),
+        ],
+    )
+    @patch("asset.use_cases.simulate.Simulate.get_bearish_purchase_price")
+    @patch("asset.use_cases.simulate.Simulate.get_bullish_purchase_price")
+    def test_process_day(
+        self,
+        mock_get_bullish_purchase_price,
+        mock_get_bearish_purchase_price,
+        date,
+        trend,
+        purchase_price,
+        number_of_assets,
+        not_invested_amount,
+    ):
+        mock_get_bullish_purchase_price.return_value = purchase_price
+        mock_get_bearish_purchase_price.return_value = purchase_price
+        simulate = Simulate(
+            {
+                "not_invested_amount": Decimal("1000"),
+                "aggregate_amount": Decimal("200"),
+                "number_of_assets": Decimal("0"),
+            }
+        )
+        simulate.data_frame = pandas.DataFrame(
+            data={"date": [pandas.Timestamp(date)], "trend": [trend]}
+        )
+        simulate.data_index = {date: 0}
+
+        date = datetime.strptime(date, "%Y-%m-%d").date()
+        simulate.process_day(date=date)
+
+        assert simulate.strategy_state["number_of_assets"] == number_of_assets
+        assert simulate.strategy_state["not_invested_amount"] == not_invested_amount
--- apps/strategy_simulator/asset/use_cases/simulate.py
+++ apps/strategy_simulator/asset/use_cases/simulate.py
@@ -1,44 +1,133 @@
-from datetime import datetime
+from decimal import Decimal
+from typing import Callable, Optional
+from datetime import datetime, timedelta
 
 import pandas
+
+from django.db.models import QuerySet
 
 from asset.repositories.asset import AssetRepository
 from asset.repositories.candle import CandleRepository
 
+from asset.use_cases.build_data_frame import BuildDataFrame
+from utils.utils.data_frame_tool import DataFameTool
+
+
+class ProcessIndexFunctions:
+    @staticmethod
+    def process_timestamp_to_date_index(value: pandas.Timestamp) -> str:
+        return str(value.to_pydatetime().date())
+
+
 class Simulate:
+    def __init__(self, strategy_state):
+        self.strategy_state = strategy_state
+        self.data_frame = None
+        self.data_index = None
 
-    def build_pandas_data_frame(self, candles):
-        data = {
-            "date": [],
-            "open": [],
-            "close": [],
-            "high": [],
-            "low": [],
-        }
+    def build_index(self, column_name: str, process_index_function: Callable) -> dict:
+        data_index = {}
+        for index in self.data_frame.index:
+            value = self.data_frame.iloc[index][column_name]
+            processed_value = process_index_function(value)
+            data_index[processed_value] = index
+        self.data_index = data_index
 
-        for candle in candles:
-            data["date"].append(candle.candle_datetime)
-            data["open"].append(candle.open_price)
-            data["close"].append(candle.close_price)
-            data["high"].append(candle.high_price)
-            data["low"].append(candle.low_price)
-                
-        data_frame = pandas.DataFrame(data=data)
-        return data_frame
-    
-    def add_ema(self, data_frame: pandas.DataFrame, ema_period: int):
-        data_frame.sort_values(by='date', inplace = True, ascending = True)
-        data_frame['ema'] = data_frame['close'].ewm(span=ema_period, adjust=False).mean()
+    def build_complete_data_frame(self, candles: QuerySet) -> pandas.DataFrame:
+        data_frame = BuildDataFrame.execute(candles=candles)
+        data_frame_tool = DataFameTool(data_frame=data_frame)
+        data_frame_tool.add_ema(
+            ema_column_name="ema", ema_period=55, reference_column_name="close"
+        )
+        data_frame_tool.add_trend(
+            number_of_registers=90, ema_column_name="ema", trend_column_name="trend"
+        )
+        data_frame_tool.add_atractive_percentage(
+            low_column_name="low",
+            ema_column_name="ema",
+            attractive_percentage_column_name="attractive_percentage",
+        )
+        data_frame_tool.add_biggest_distances(
+            number_of_registers=30,
+            low_column_name="low",
+            high_column_name="high",
+            biggest_high_column_name="biggest_high",
+            lowest_low_column_name="lowest_low",
+        )
+        return data_frame_tool.data_frame
 
-    
+    def get_bullish_purchase_price(self, row) -> Optional[Decimal]:
+        aux_buyer_price = row["ema"] * (
+            1 + self.strategy_state["bullish_attractive_percentage"]
+        )
 
-    def execute(self, ticker: str, ema_period: int, from_date: datetime, end_date: datetime):
+        if row["low"] <= aux_buyer_price <= row["high"]:
+            if row["open"] <= aux_buyer_price:
+                final_buyer_price = row["open"]
+            else:
+                final_buyer_price = aux_buyer_price
+        elif row["high"] < aux_buyer_price:
+            final_buyer_price = row["open"]
+        else:
+            final_buyer_price = None
+        return final_buyer_price
+
+    def get_bearish_purchase_price(self, row) -> Optional[Decimal]:
+        aux_buyer_price = row["ema"] * (
+            1 + self.strategy_state["bearish_attractive_percentage"]
+        )
+
+        if row["low"] <= aux_buyer_price <= row["high"]:
+            if row["open"] <= aux_buyer_price:
+                final_buyer_price = row["open"]
+            else:
+                final_buyer_price = aux_buyer_price
+        elif row["high"] < aux_buyer_price:
+            final_buyer_price = row["open"]
+        else:
+            final_buyer_price = None
+        return final_buyer_price
+
+    def get_number_of_assets_to_buy(self, purchase_price: Decimal):
+        return round(self.strategy_state["not_invested_amount"] / purchase_price, 2)
+
+    def process_day(self, date: datetime.date):
+        if date.weekday() == 6:
+            self.strategy_state["not_invested_amount"] += self.strategy_state[
+                "aggregate_amount"
+            ]
+
+        index_row = self.data_index.get(str(date))
+        if index_row is not None:
+            row = self.data_frame.iloc[index_row]
+            if row["trend"] == "bullish":
+                purchase_price = self.get_bullish_purchase_price(row=row)
+            elif row["trend"] == "bearish":
+                purchase_price = self.get_bearish_purchase_price(row=row)
+
+        if purchase_price is not None:
+            number_of_assets = self.get_number_of_assets_to_buy(
+                purchase_price=purchase_price
+            )
+            self.strategy_state["number_of_assets"] = number_of_assets
+            self.strategy_state["not_invested_amount"] = Decimal("0")
+
+    def execute(self, ticker: str, from_date: datetime, end_date: datetime):
         asset = AssetRepository.get_asset({"ticker": ticker})
-        candles = CandleRepository.get_candles({
-            "asset_id": asset.id,
-            "candle_datetime__range": [from_date, end_date],
-            "periodicity": "daily"
-        })
-        data_frame = self.build_pandas_data_frame(candles=candles)
-        self.add_ema(data_frame=data_frame, ema_period=ema_period)
-        
+        candles = CandleRepository.get_candles(
+            {
+                "asset_id": asset.id,
+                "candle_datetime__range": [from_date, end_date],
+                "periodicity": "daily",
+            }
+        )
+        self.data_frame = self.build_complete_data_frame(candles=candles)
+        self.build_index(
+            column_name="date",
+            process_index_function=ProcessIndexFunctions.process_timestamp_to_date_index,
+        )
+
+        aux_date = from_date
+        while aux_date <= end_date:
+            self.process_day(date=aux_date)
+            aux_date += timedelta(days=1)
